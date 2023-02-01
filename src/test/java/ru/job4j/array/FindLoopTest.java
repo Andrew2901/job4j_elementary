5f038b4cf10fcc42737248a1426fcedb6d3abf53
Andrew2901 <confessor290198@gmail.com>
2023-02-01 19:16:55 +0500

    6.5. Поиск индекса в диапазоне. [#156318]

--- src/test/java/ru/job4j/array/FindLoopTest.java
+++ src/test/java/ru/job4j/array/FindLoopTest.java
@@ -2,6 +2,7 @@ package ru.job4j.array;
 import org.junit.jupiter.api.Test;
 import static org.assertj.core.api.Assertions.assertThat;
 public class FindLoopTest {
+
     @Test
     public void whenArrayHas5Then0() {
         int[] data = new int[] {5, 10, 3};
@@ -10,6 +11,7 @@ public class FindLoopTest {
         int expected = 0;
         assertThat(result).isEqualTo(expected);
     }
+
     @Test
     public void whenArrayHasNot10ThenMinus1() {
         int[] data = new int[] {5, 11, 3};
