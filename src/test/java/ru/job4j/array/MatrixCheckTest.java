2fe446707834c157dca910b6c0fc0c708ad39045
Andrew2901 <confessor290198@gmail.com>
2023-02-05 13:00:54 +0500

    6.7.4. Выигрышные комбинации в сокобан [#53859]

--- src/test/java/ru/job4j/array/MatrixCheckTest.java
+++ src/test/java/ru/job4j/array/MatrixCheckTest.java
@@ -1,8 +1,6 @@
 package ru.job4j.array;
-
 import org.junit.jupiter.api.Test;
 import static org.assertj.core.api.Assertions.assertThat;
-
 public class MatrixCheckTest {
     @Test
     public void whenHasMonoHorizontal() {
@@ -15,7 +13,6 @@ public class MatrixCheckTest {
         boolean result = MatrixCheck.monoHorizontal(input, row);
         assertThat(result).isTrue();
     }
-
     @Test
     public void whenHasNoMonoHorizontal() {
         char[][] input = {
@@ -27,7 +24,6 @@ public class MatrixCheckTest {
         boolean result = MatrixCheck.monoHorizontal(input, row);
         assertThat(result).isFalse();
     }
-
     @Test
     public void whenHasMonoVertical() {
         char[][] input = {
@@ -39,7 +35,6 @@ public class MatrixCheckTest {
         boolean result = MatrixCheck.monoVertical(input, column);
         assertThat(result).isTrue();
     }
-
     @Test
     public void whenHasNoMonoVertical() {
         char[][] input = {
@@ -51,7 +46,6 @@ public class MatrixCheckTest {
         boolean result = MatrixCheck.monoVertical(input, column);
         assertThat(result).isFalse();
     }
-
     @Test
     public void whenDiagonalFullX() {
         char[][] input = {
@@ -63,7 +57,6 @@ public class MatrixCheckTest {
         char[] expected = {'X', 'X', 'X'};
         assertThat(result).containsExactly(expected);
     }
-
     @Test
     public void whenDiagonalFullOne() {
         char[][] input = {
@@ -75,7 +68,6 @@ public class MatrixCheckTest {
         char[] expected = {'1', '1', '1'};
         assertThat(result).containsExactly(expected);
     }
-
     @Test
     public void whenDiagonalMix() {
         char[][] input = {
@@ -86,5 +78,57 @@ public class MatrixCheckTest {
         char[] result = MatrixCheck.extractDiagonal(input);
         char[] expected = {'X', 'Y', 'Z'};
         assertThat(result).containsExactly(expected);
+    }
+
+    @Test
+    public void whenDataMonoByTrueThenTrue() {
+        char[][] input = {
+                {' ', ' ', 'X', ' ', ' '},
+                {' ', ' ', 'X', ' ', ' '},
+                {' ', ' ', 'X', ' ', ' '},
+                {' ', ' ', 'X', ' ', ' '},
+                {' ', ' ', 'X', ' ', ' '},
+        };
+        boolean result = MatrixCheck.isWin(input);
+        assertThat(result).isTrue();
+    }
+
+    @Test
+    public void whenDataNotMonoByTrueThenFalse() {
+        char[][] input = {
+                {' ', ' ', 'X', ' ', ' '},
+                {' ', ' ', 'X', ' ', ' '},
+                {' ', 'X', ' ', ' ', ' '},
+                {' ', ' ', 'X', ' ', ' '},
+                {' ', ' ', 'X', ' ', ' '},
+        };
+        boolean result = MatrixCheck.isWin(input);
+        assertThat(result).isFalse();
+    }
+
+    @Test
+    public void whenDataHMonoByTrueThenTrue() {
+        char[][] input = {
+                {' ', ' ', ' ', ' ', ' '},
+                {' ', ' ', ' ', ' ', ' '},
+                {'X', 'X', 'X', 'X', 'X'},
+                {' ', ' ', 'X', ' ', ' '},
+                {' ', ' ', 'X', ' ', ' '},
+        };
+        boolean result = MatrixCheck.isWin(input);
+        assertThat(result).isTrue();
+    }
+
+    @Test
+    public void whenDataNotMonoByFalseThenFalse() {
+        char[][] input = {
+                {' ', ' ', ' ', ' ', ' '},
+                {' ', ' ', ' ', ' ', ' '},
+                {'X', ' ', 'X', 'X', 'X'},
+                {' ', ' ', 'X', ' ', ' '},
+                {' ', ' ', 'X', ' ', ' '},
+        };
+        boolean result = MatrixCheck.isWin(input);
+        assertThat(result).isFalse();
     }
 }
